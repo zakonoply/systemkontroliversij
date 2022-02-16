adca6fe0861049b01dad7c1b19c46269da27d83c
zakonoply <99838804+zakonoply@users.noreply.github.com>
2022-02-16 22:45:35 +0200

    №1
    pierwsza wersja kodu z programowania

---
+++ cw7.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <cmath> 
+ 
+using namespace std;
+ 
+int main()
+{
+  double a,b,c,d,x1,x2; 
+ 
+  cout << "napisz a\n"; 
+  cin >> a;
+  cout << "napisz b\n";
+  cin >> b;
+  cout << "napisz c\n";
+  cin >> c;
+ 
+  d = b * b - 4 * a * c; 
+  if (d > 0) 
+  {
+    x1 = ((-b) + sqrt(d)) / (2 * a);
+    x2 = ((-b) - sqrt(d)) / (2 * a);
+    cout << "x1 = " << x1 << "\n";
+    cout << "x2 = " << x2 << "\n";
+  }
+  if (d == 0)
+  {
+    x1 = -(b / (2 * a));
+    cout << "x1 = x2 = " << x1 << "\n";
+  }
+  if (d < 0) 
+    cout << "Bl¹d";
+}
+
